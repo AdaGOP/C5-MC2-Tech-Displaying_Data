71150e367bfae84ff54c6311967c125cc4e427b3
Haryanto Quang <apple.haryanto@gmail.com>
2022-03-29 00:10:27 +0700

    create static collection view cell

--- Photo_Gallery/ViewController.swift
+++ Photo_Gallery/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
+    let cellIDs = ["CardCell", "CardCell2", "CardCell3"]
+    let cellSizes = Array(repeatElement(CGSize(width:130, height:130), count: 3))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cardCollectionView.delegate = self
+        cardCollectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellIDs.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return cardCollectionView.dequeueReusableCell(withReuseIdentifier: cellIDs[indexPath.item], for: indexPath)
+    }
+}
+
+extension ViewController: UICollectionViewDelegate{
 
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSizes[indexPath.item]
+    }
+}
