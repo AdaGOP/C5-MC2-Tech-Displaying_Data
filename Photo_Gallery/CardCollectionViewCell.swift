c6f5e8ec278388c965ecde4734bcd3a3685c07cc
Haryanto Quang <apple.haryanto@gmail.com>
2022-03-30 01:33:20 +0700

    add static collection cells using a diffabledatasource

---
+++ Photo_Gallery/CardCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardCollectionViewCell.swift
+//  Photo_Gallery
+//
+//  Created by Haryanto Salim on 29/03/22.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+}
--- Photo_Gallery/ViewController.swift
+++ Photo_Gallery/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CardSection: Int {
+    case main
+}
 
+struct CardImage: Hashable {
+    var image: UIImage
+    var title: String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<CardSection, CardImage>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<CardSection, CardImage>
+    
+    private lazy var dataSource = makeDataSource()
+    
+    let cardCells = [CardImage(image: UIImage(named: "josue-michel")!, title: "josue-michel"),CardImage(image: UIImage(named: "dim-hou-tim-john")!, title: "dim-hou-tim-john")]
+    
+    let cellSizes = Array(repeatElement(CGSize(width:130, height:130), count: 2))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cardCollectionView.delegate = self
+        applySnapshot(animatingDifferences: false)
     }
-
-
+    
+    func makeDataSource() -> DataSource {
+        let dataSource = DataSource(
+            collectionView: cardCollectionView,
+            cellProvider: { (collectionView, indexPath, card) ->
+                UICollectionViewCell? in
+                let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: "CardCell",
+                    for: indexPath) as? CardCollectionViewCell
+                
+                cell?.cellImage.image = self.cardCells[indexPath.item].image
+                cell?.cellTitle.text = self.cardCells[indexPath.item].title
+                return cell
+            })
+        return dataSource
+    }
+    
+    func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([CardSection.main])
+        snapshot.appendItems(cardCells, toSection: CardSection.main)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return cellSizes[indexPath.item]
+    }
+}
